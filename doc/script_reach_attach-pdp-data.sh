64bf927f96d5dcced5daa291ff80b2a268739fa2
tomislav miksa <tomislav.miksa@gmail.com>
2026-05-08 17:14:41 +0200

    improved script with infinite loop

--- doc/script_reach_attach-pdp-data.sh
+++ doc/script_reach_attach-pdp-data.sh
@@ -17,15 +17,17 @@
 50:at:AT+CGDCONT?:data.apn.name:3:goto 60:
 51:at:AT+CGDCONT=1,"IP","data.apn.name"
 
-60:at:AT+CREG=2:::
+60:at:AT+CREG=2::::
 61:at:AT+CREG?:2,(1|5):20::goto 40
 70:at:AT+QIACT?:\.::goto 80:
 71:at:AT+QIACT=1::::
-72:sh:sleep 10:::
-80:at:AT+CCLK?:::
+72:sh:sleep 10::::
+80:at:AT+CCLK?::::
 
-90:at:AT+QIOPEN=1,0,"TCP","postman-echo.com",80,1234,0:::
-91:sh:sleep 3:::
-92:at:AT+QISTATE=1,0:::
-93:at:AT+QICLOSE=1:::
+90:at:AT+QIOPEN=1,0,"TCP","postman-echo.com",80,1234,0::::
+91:sh:sleep 3::::
+92:at:AT+QISTATE=1,0::::
+93:at:AT+QICLOSE=1::::
+
+100:sh:sleep 30:::goto 1:
 
